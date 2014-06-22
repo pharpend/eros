dd3c096cef9d48d1443f74f0ccf84111c8a29301
Peter Harpending <pharpend2@gmail.com>
2014-06-22 13:45:45 -0600

    Initial commit - skeleton project.

---
+++ skel/Skel.hs
@@ -0,0 +1,11 @@
+-- |
+-- Module       : Skel
+-- Description  : A module for Tropical whatever
+-- Copyright    : Copyright © 2014 Valkyrian Industries
+-- License      : BSD3
+-- Maintainer   : Peter Harpending <peter@valkyrian.com>
+-- Stability    : experimental
+-- Portability  : archlinux
+--
+
+module Skel where
